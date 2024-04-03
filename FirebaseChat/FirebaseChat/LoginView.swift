a71a35283577af5709fbfaf1f71acc4a4971e81d
Travis Lu <ahyo6127@gmail.com>
2024-04-03 20:51:42 +0800

    Save Images to Firebase Storage
    1. Add Storage with Firebase
    2. Add ImagePicker

--- FirebaseChat/FirebaseChat/LoginView.swift
+++ FirebaseChat/FirebaseChat/LoginView.swift
@@ -7,11 +7,13 @@
 
 import SwiftUI
 import Firebase
+import FirebaseStorage
 
 
 class FirebaseManager: NSObject {
     
     let auth: Auth
+    let storage: Storage
     
     static let shared = FirebaseManager()
     
@@ -19,6 +21,7 @@ class FirebaseManager: NSObject {
         FirebaseApp.configure()
         
         self.auth = Auth.auth()
+        self.storage = Storage.storage()
         
         super.init()
     }
@@ -29,6 +32,8 @@ struct LoginView: View {
     @State var isLoginMode = false
     @State var email = ""
     @State var password = ""
+    
+    @State var shouldShowImagePicker = false
 
     var body: some View {
         NavigationView {
@@ -45,11 +50,29 @@ struct LoginView: View {
                     
                     if !isLoginMode {
                         Button {
-                            
+                            shouldShowImagePicker.toggle()
                         } label: {
-                                Image(systemName: "person.fill")
-                                    .font(.system(size: 64))
-                                    .padding()
+                            
+                            VStack {
+                                
+                                if let image = self.image {
+                                    Image(uiImage: image)
+                                        .resizable()
+                                        .frame(width: 128, height: 128)
+                                        .scaledToFill()
+                                        .cornerRadius(64)
+                                    
+                                } else {
+                                    Image(systemName: "person.fill")
+                                        .font(.system(size: 64))
+                                        .padding()
+                                        .foregroundStyle(Color.black)
+                                }
+                            }
+                            .overlay(RoundedRectangle(cornerRadius: 64)
+                                .stroke(Color.black, lineWidth: 2))
+                            
+                               
                         }
                     }
                     
@@ -84,12 +107,16 @@ struct LoginView: View {
             }
             .navigationTitle(isLoginMode ? "Log In" : "Create Account")
             .background(Color(.init(white: 0, alpha: 0.05))
-//                .ignoresSafeArea()
+            .ignoresSafeArea()
             )
         }
 //        .navigationViewStyle(StackNavigationViewStyle())
+        .fullScreenCover(isPresented: $shouldShowImagePicker, onDismiss: nil) {
+            ImagePicker(image: $image)
+        }
     }
     
+    @State var image: UIImage?
     
     private func handleAction() {
         if isLoginMode {
@@ -124,6 +151,32 @@ struct LoginView: View {
             }
             print("Successfully created user: \(result?.user.uid ?? "")")
             self.loginStatusMessage = "Successfully created user: \(result?.user.uid ?? "")"
+            
+            self.persistImageToStorage()
+        }
+    }
+    
+    private func persistImageToStorage() {
+//        let filename = UUID().uuidString
+        
+        
+        guard let uid = FirebaseManager.shared.auth.currentUser?.uid else { return }
+        let ref = FirebaseManager.shared.storage.reference(withPath: uid)
+        guard let imageData = self.image?.jpegData(compressionQuality: 0.5) else { return }
+        ref.putData(imageData, metadata: nil) { metadata, err in
+            if let err = err {
+                self.loginStatusMessage = "Failed to push image to Storge: \(err)"
+                return
+            }
+            
+            ref.downloadURL { url, err in
+                if let err = err {
+                    self.loginStatusMessage = "Failed to retrieve downloadURL: \(err)"
+                    return
+                }
+                
+                self.loginStatusMessage = "Successfully stored image with url: \(url?.absoluteString ?? "")"
+            }
         }
     }
 }
